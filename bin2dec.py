374b1658b96b7926cfb3ddbad6d3270d007dda79
Caio Rodrigues <rfcaio90@gmail.com>
2021-04-26 11:34:14 -0300

    add binary string validation

--- bin2dec.py
+++ bin2dec.py
@@ -1,4 +1,10 @@
+import re
+
+
 def bin2dec(binary_string):
+    if not re.match('^[0-1]+$', binary_string):
+        raise ValueError('Invalid binary string.')
+
     exponent = len(binary_string) - 1
     result = 0
     for digit in binary_string:
--- test_bin2dec.py
+++ test_bin2dec.py
@@ -7,3 +7,6 @@ class Bin2DecTest(unittest.TestCase):
     def test_bin2dec(self):
         self.assertEqual(bin2dec('101111'), 47)
         self.assertEqual(bin2dec('10111100'), 188)
+
+        with self.assertRaises(ValueError):
+            bin2dec('1a1')
