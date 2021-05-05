117d9b456b940f7c62851585db8d18cfc36269a5
Caio Rodrigues <rfcaio90@gmail.com>
2021-05-05 20:57:17 -0300

    improve invalid binary string test

--- test_bin2dec.py
+++ test_bin2dec.py
@@ -4,9 +4,14 @@ from bin2dec import bin2dec
 
 
 class Bin2DecTest(unittest.TestCase):
+    def test_invalid_binary_string(self):
+        with self.assertRaises(ValueError) as invalid_binary_string_err:
+            bin2dec('1a1')
+        self.assertEqual(
+            str(invalid_binary_string_err.exception),
+            'Invalid binary string.'
+        )
+
     def test_bin2dec(self):
         self.assertEqual(bin2dec('101111'), 47)
         self.assertEqual(bin2dec('10111100'), 188)
-
-        with self.assertRaises(ValueError):
-            bin2dec('1a1')
