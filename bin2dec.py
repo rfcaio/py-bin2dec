9f006e6d7c0e3a0e5213539b4bf85594658a52c1
Caio Rodrigues <rfcaio90@gmail.com>
2021-05-05 21:30:05 -0300

    add type validation for bin2dec function

--- bin2dec.py
+++ bin2dec.py
@@ -1,15 +1,18 @@
 import re
 
 
-def bin2dec(binary_string):
+def bin2dec(value):
     VALID_BINARY_STRING = r'^[0-1]+$'
 
-    if not re.match(VALID_BINARY_STRING, binary_string):
+    if type(value) != str:
+        raise ValueError('Invalid binary type.')
+
+    if not re.match(VALID_BINARY_STRING, value):
         raise ValueError('Invalid binary string.')
 
-    exponent = len(binary_string) - 1
+    exponent = len(value) - 1
     result = 0
-    for digit in binary_string:
+    for digit in value:
         result += int(digit) * 2 ** exponent
         exponent -= 1
     return result
--- test_bin2dec.py
+++ test_bin2dec.py
@@ -4,6 +4,14 @@ from bin2dec import bin2dec
 
 
 class Bin2DecTest(unittest.TestCase):
+    def test_invalid_binary_type(self):
+        with self.assertRaises(ValueError) as invalid_binary_type_err:
+            bin2dec(101)
+        self.assertEqual(
+            str(invalid_binary_type_err.exception),
+            'Invalid binary type.'
+        )
+
     def test_invalid_binary_string(self):
         with self.assertRaises(ValueError) as invalid_binary_string_err:
             bin2dec('1a1')
